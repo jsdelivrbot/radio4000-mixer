009f44ceb89b2c43e54f8cb3fbd8bc01047f931b
Oskar Rough <oskar@rough.dk>
2017-04-12 18:57:31 +0200

    Two players and a crossfader

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,57 @@
 <template>
   <div id="app">
-    <img src="http://vuejs.org/images/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="Decks">
+      <Radio4000Player slug="sugar-hiccup" :volume="balance" />
+      <Radio4000Player slug="200ok" :volume="balance" />
+    </div>
+    <input type="range" v-model.number="balance">
+    <p>{{balance}}</p>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import Radio4000Player from 'radio4000-player/src/App.vue'
+
 export default {
-  name: 'app',
-  data () {
+	name: 'youtube-mixer',
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      balance: 50
     }
-  }
+  },
+	components: {
+		Radio4000Player 
+	}
 }
 </script>
 
+<style>
+body {
+  margin: 0;
+}
+</style>
+
 <style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  padding: 0 0.5em 1em;
+}
+.Decks {
+  display: flex;
+  margin-bottom: 1em;
+}
+.Decks > * {
+  flex: 50%;
+ max-width: none;
+ border: 1px solid;
+ border-width: 1em 0.5em;
+}
+input[type="range"] {
+  display: block;
+  width: 80%;
+  margin: 0 auto;
+}
+p {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-img {
-  width: 200px;
-  height: 200px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
