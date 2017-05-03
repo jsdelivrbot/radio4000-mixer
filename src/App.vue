b598b2cc19576bfd3ad21f42e8784ae60aeddbda
Oskar Rough <oskar@rough.dk>
2017-05-03 18:31:56 +0200

    Fix volume/balance

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,59 @@
 <template>
-  <div id="app">
-    <div class="Decks">
-      <Radio4000Player slug="sugar-hiccup" :volume="balance" />
-      <Radio4000Player slug="200ok" :volume="balance" />
-    </div>
-    <input type="range" v-model.number="balance">
-    <p>{{balance}}</p>
-  </div>
+	<div id="app">
+		<div class="Decks">
+			<radio4000-player slug="sugar-hiccup" :volume="volA"></radio4000-player>
+			<radio4000-player slug="200ok" :volume="volB"></radio4000-player>
+		</div>
+		<input type="range" v-model.number="balance">
+		<p>{{balance}}</p>
+	</div>
 </template>
 
 <script>
-import Vue from 'vue'
-import Radio4000Player from 'radio4000-player/src/App.vue'
-
 export default {
 	name: 'youtube-mixer',
-  data() {
-    return {
-      balance: 50
-    }
-  },
-	components: {
-		Radio4000Player 
+	data() {
+		return {
+			balance: 50
+		}
+	},
+	computed: {
+		volA() {
+			return this.balance * -1 + 100
+		},
+		volB() {
+			return this.balance
+		}
 	}
 }
 </script>
 
 <style>
 body {
-  margin: 0;
+	margin: 0;
 }
 </style>
 
 <style scoped>
 #app {
-  padding: 0 0.5em 1em;
+	padding: 0 0.5em 1em;
 }
 .Decks {
-  display: flex;
-  margin-bottom: 1em;
+	display: flex;
+	margin-bottom: 1em;
 }
 .Decks > * {
-  flex: 50%;
- max-width: none;
- border: 1px solid;
- border-width: 1em 0.5em;
+	flex: 50%;
+	max-width: none;
+	border: 1px solid;
+	border-width: 1em 0.5em;
 }
 input[type="range"] {
-  display: block;
-  width: 80%;
-  margin: 0 auto;
+	display: block;
+	width: 80%;
+	margin: 0 auto;
 }
 p {
-  text-align: center;
+	text-align: center;
 }
 </style>
